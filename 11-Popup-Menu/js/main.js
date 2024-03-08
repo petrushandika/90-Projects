46dc9dcc2094b1d3bbee210b8e635e1436cc16bb
petrushandika <petrushandikasinaga@gmail.com>
2024-03-08 16:04:31 +0700

    Day 10

---
+++ 11-Popup-Menu/js/main.js
@@ -0,0 +1,9 @@
+function openButton() {
+    const popup = document.querySelector('.popup');
+    popup.classList.add('open-popup');
+}
+
+function closeButton() {
+    const popup = document.querySelector('.popup');
+    popup.classList.remove('open-popup');
+}
